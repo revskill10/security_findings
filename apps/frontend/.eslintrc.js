e5e9bf621293f359d8355932707847cab91041ad
Truong Hoang Dung <truonghoangdung1987hp@gmail.com>
2022-06-10 12:35:06 +0700

    fix eslint and generate openapi docs

--- apps/frontend/.eslintrc.js
+++ apps/frontend/.eslintrc.js
@@ -1,14 +1,8 @@
 module.exports = {
   "extends": ["react-app", "custom"],
-  "rules": {
-    "additional-rule": "warn"
-  },
   "overrides": [
     {
-      "files": ["**/*.(j|t)s?(x)"],
-      "rules": {
-        "additional-typescript-only-rule": "warn"
-      }
+      "files": ["**/*.(j|t)s?(x)"]
     }
   ]
 }
