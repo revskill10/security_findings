215a2d82bdc772e19633464079c073dbf0f3d333
Truong Hoang Dung <truonghoangdung1987hp@gmail.com>
2022-06-13 19:47:16 +0700

    Rename client

--- packages/items_service_client/orval.config.js
+++ packages/items_service_client/orval.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-    petstore: {
+    react_query_client: {
       output: {
         mode: 'tags-split',
         target: 'src/index.ts',
         schemas: 'src/model',
         client: 'react-query',
-        mock: true,
+        mock: false,
       },
       input: {
         target: './docs/openapi.yaml',
