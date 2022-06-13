96aa62b347006944418d67d14ce8d2f4f6f2761a
Truong Hoang Dung <truonghoangdung1987hp@gmail.com>
2022-06-13 14:27:08 +0700

    Fix storybook

--- packages/ui-library/src/stories/new-item-form.stories.tsx
+++ packages/ui-library/src/stories/new-item-form.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 import 'semantic-ui-css/semantic.min.css';
-import { NewItemForm } from '../new-item-form';
+import { NewItemForm } from '../components/new-item-form';
 export default {
     title: 'New Item Form',
     component: NewItemForm,
